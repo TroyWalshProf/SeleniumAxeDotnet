a373464f0d889e646b60b5c98d3f0b4ecf0fe83b
Javier Novoa <javier.novoa@outlook.com>
2017-03-31 21:57:09 -0500

    Add web driver extensions

--- Globant.Selenium.Axe/Globant.Selenium.Axe/WebDriverExtensions.cs
+++ Globant.Selenium.Axe/Globant.Selenium.Axe/WebDriverExtensions.cs
@@ -1,7 +1,29 @@
-﻿namespace Globant.Selenium.Axe
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Globant.Selenium.Axe
 {
     public static class WebDriverExtensions
     {
-        
+        public static AxeResult Analyze(this IWebDriver webDriver)
+        {
+            if (webDriver == null)
+                throw new ArgumentNullException(nameof(webDriver));
+
+            AxeBuilder axeBuilder = new AxeBuilder(webDriver);
+            return axeBuilder.Analyze();
+        }
+
+        public static AxeResult Analyze(this IWebDriver webDriver, IWebElement context)
+        {
+            if (webDriver == null)
+                throw new ArgumentNullException(nameof(webDriver));
+
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            AxeBuilder axeBuilder = new AxeBuilder(webDriver);
+            return axeBuilder.Analyze(context);
+        }
     }
 }
