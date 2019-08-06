6d1d102a8af01165d445cbf8b4bd0d71611a5c54
Dan Bjorge <danielbj@microsoft.com>
2019-08-06 15:42:07 -0700

    Use .NET core compatible form of EmbeddedResources

--- Selenium.Axe/Selenium.Axe/EmbeddedResourceAxeProvider.cs
+++ Selenium.Axe/Selenium.Axe/EmbeddedResourceAxeProvider.cs
@@ -1,9 +1,17 @@
-﻿using Selenium.Axe.Properties;
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
 
 namespace Selenium.Axe
 {
     internal class EmbeddedResourceAxeProvider : IAxeScriptProvider
     {
-        public string GetScript() => Resources.axe_min;
+        public string GetScript() {
+            var assembly = Assembly.GetAssembly(typeof(EmbeddedResourceAxeProvider));
+            var resourceStream = assembly.GetManifestResourceStream("Selenium.Axe.Resources.axe.min.js");
+            using (var reader = new StreamReader(resourceStream, Encoding.UTF8)) {
+                return reader.ReadToEnd();
+            }
+        }
     }
 }
