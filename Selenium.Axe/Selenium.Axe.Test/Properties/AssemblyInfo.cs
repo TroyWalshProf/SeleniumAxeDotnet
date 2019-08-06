c0eb7b9267bc1384be21c4663376da56876d8d3a
Dan Bjorge <danielbj@microsoft.com>
2019-08-05 17:21:24 -0700

    Remove unnecessary AssemblyInfo

--- Selenium.Axe/Selenium.Axe.Test/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("7d5db0a8-263e-40d4-90c8-b86ba672c783")]
